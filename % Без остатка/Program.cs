7a3eeebfe953803d58f4c39b60fd6bc2209eecbf
OksanaKovalskaya <ksunia-nia@yandex.ru>
2022-04-19 21:47:20 +0300

    деление без остатка

---
+++ % Без остатка/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine ("Введите трехзначное число и нажмите Enter");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine ($"Последняя цифра указанного числа {N}");
+Console.WriteLine (N % 10);
